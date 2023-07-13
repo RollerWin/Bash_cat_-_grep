b91ab7a1b3fc0d1e38d9dcd74d55afa7b14ceaec
Florentina Aiko <florenta@un-m1.kzn.21-school.ru>
2023-07-13 07:53:36 +0300

    Ended Cat Function

--- src/cat/input.c
+++ src/cat/input.c
@@ -1,65 +1,139 @@
 #include "input.h"
-#include <stdio.h>
-#include <string.h>
+
 #include <getopt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-enum Cat_flags {
-  // NON_EMPTY_LINE_NUMBERING_FLAG = 'b',
-  // DOLLAR_DISPLAY_FLAG = 'e',
-  LINE_NUMBERING_FLAG = 'n',
-  // STRING_COMPRESSION_FLAG = 's',
-  // TAB_REPLACEMENT_FLAG = 't',
-  UNKNOWN_COMMAND_FLAG = '?'
-};
+const char* wrong_file_message = ": No such file or directory";
+const char* cat_message = "cat: ";
+const int text_string_length = 1024;
+const int file_name_length = 64;
 
-void input_args(int argc, char *argv[]) 
-{
-  int user_input;
+void read_console(int first_file_position, int argc, char* argv[]) {
+  char text_string[text_string_length];
 
-  //while ((user_input = getopt(argc, argv, "b:ens:t:")) != -1) {
-  for (int i = 1; i < argc && ((user_input = getopt(argc, argv, "b:e:n:s:t:")) != -1); i++) 
-  {
-    switch (user_input)
-    {
-    case LINE_NUMBERING_FLAG:
-      output_files(argv[i+1]); 
-      i++;
-      break;
-    
-    case UNKNOWN_COMMAND_FLAG:
-      printf("мдаааа");   
-      break;
+  for (int i = first_file_position; i < argc; i++) {
+    FILE* user_file;
+
+    if ((user_file = fopen(argv[i], "r")) == NULL) {
+      choose_error_stream(first_file_position, argv[i]);
+    } else {
+      detect_flag(argc, argv, text_string, user_file);
+      fclose(user_file);
     }
   }
 }
 
-void output_files(char* argv) {
-  const char* error_message = "No such file or directory";
-  const char* cat_message = "cat";
-  const int text_string_length = 2048;
-  const int file_name_length = 80;
-  
-  char file_name[file_name_length];
-  char text_string[text_string_length];
+void choose_error_stream(int first_file_position, char* argv) {
+  char error_message[file_name_length];
+  build_error_message(argv, error_message);
+  if (first_file_position == 1) {
+    print_default_error_message(error_message);
+  } else {
+    print_flag_error_message(error_message);
+  }
+}
 
-  FILE* user_file;
-  
-  strcpy(file_name, argv);
+void print_default_error_message(char* error_message) {
+  printf("%s\n", error_message);
+}
+void print_flag_error_message(char* error_message) { perror(error_message); }
 
-    if ((user_file = fopen(file_name, "r")) == NULL) 
-    {
-      printf("%s: %s: %s\n", cat_message, file_name, error_message);
-    } 
-    else 
-    {
-      int string_number = 1;
+char* build_error_message(char* file_name, char* error_message) {
+  strcat(error_message, cat_message);
+  strcat(error_message, file_name);
+  strcat(error_message, wrong_file_message);
+  return error_message;
+}
 
-      while (fgets(text_string, text_string_length, user_file)) 
-      {
-        printf("%6d  %s", string_number, text_string);
-        string_number++;
+void detect_flag(int argc, char* argv[], char* text_string, FILE* user_file) {
+  switch (getopt(argc, argv, "b:e:n:s:t:")) {
+    case LINE_NUMBERING_FLAG:
+      line_numbering_funtion(text_string, user_file);
+      break;
+
+    case NON_EMPTY_LINE_NUMBERING_FLAG:
+      non_empty_line_numbering_funtion(text_string, user_file);
+      break;
+
+    case DOLLAR_DISPLAY_FLAG:
+      dollar_display_function(text_string, user_file);
+      break;
+
+    case STRING_COMPRESSION_FLAG:
+      string_compression_function(text_string, user_file);
+      break;
+
+    case TAB_REPLACEMENT_FLAG:
+      tab_replacement_function(text_string, user_file);
+      break;
+
+    default:
+      default_print_files(text_string, user_file);
+      break;
+  }
+}
+
+void line_numbering_funtion(char* text_string, FILE* user_file) {
+  int string_number = 1;
+
+  while (fgets(text_string, text_string_length, user_file)) {
+    printf("%6d  %s", string_number, text_string);
+    string_number++;
+  }
+}
+
+void non_empty_line_numbering_funtion(char* text_string, FILE* user_file) {
+  int string_number = 1;
+
+  while (fgets(text_string, text_string_length, user_file)) {
+    if (text_string[0] == '\n') {
+      printf("%6c  %s", ' ', text_string);
+    } else {
+      printf("%6d  %s", string_number, text_string);
+      string_number++;
+    }
+  }
+}
+
+void dollar_display_function(char* text_string, FILE* user_file) {
+  while (fgets(text_string, text_string_length, user_file)) {
+    if (text_string[strlen(text_string) - 1] == '\n') {
+      text_string[strlen(text_string) - 1] = '$';
+    }
+    printf("%s\n", text_string);
+  }
+}
+
+void string_compression_function(char* text_string, FILE* user_file) {
+  int empty_string_counter;
+
+  while (fgets(text_string, text_string_length, user_file)) {
+    if (text_string[0] == '\n' && empty_string_counter == 0) {
+      printf("%s", text_string);
+      empty_string_counter = 1;
+    } else if (text_string[0] != '\n' || empty_string_counter == 0) {
+      empty_string_counter = 0;
+      printf("%s", text_string);
+    }
+  }
+}
+
+void tab_replacement_function(char* text_string, FILE* user_file) {
+  while (fgets(text_string, text_string_length, user_file)) {
+    for (int j = 0; j < (int)strlen(text_string); j++) {
+      if (text_string[j] != '\t') {
+        printf("%c", text_string[j]);
+      } else {
+        printf("^I");
       }
     }
+  }
+}
 
-    fclose(user_file);
+void default_print_files(char* text_string, FILE* user_file) {
+  while (fgets(text_string, text_string_length, user_file)) {
+    printf("%s", text_string);
+  }
 }
--- src/cat/main.c
+++ src/cat/main.c
@@ -1,40 +1,39 @@
 #include "input.h"
 
+#include <string.h>
+#include <time.h>
+
 int main(int argc, char *argv[]) {
-  input_args(argc, argv);
 
-  // while ((user_input = getopt(argc, argv, "b:ens:t:")) != -1) {
-  //   switch (user_input) {
-  //     case NON_EMPTY_LINE_NUMBERING_FLAG:
-  //       printf("Выбран верный верный флаг b = %s.\n", optarg);
-  //       break;
+//------------------------------------
+  // struct timespec mt1, mt2;
+  // long int tt;
+  // clock_gettime(CLOCK_REALTIME, &mt1);
 
-  //     case DOLLAR_DISPLAY_FLAG:
-  //       printf("Выбран верный верный флаг e.\n");
-  //       break;
+  // printf("Секунды : %ld\n", mt1.tv_sec);
+  // printf("Наносекунды : %ld\n", mt1.tv_nsec);
+//------------------------------------
 
-  //     case LINE_NUMBERING_FLAG:
-  //       if (optarg == (char *)"пипа") {
-  //         printf("Выбран верный верный флаг n.\n");
-  //       } else {
-  //         printf("Неверный ввод!");
-  //       }
+  int min_elements_numbers_with_flag = 2;
+  int flag_position = 1;
+  size_t flag_length = 2;
 
-  //       break;
+  int first_file_position;
 
-  //     case STRING_COMPRESSION_FLAG:
-  //       printf("Выбран верный верный флаг s = %s.\n", optarg);
-  //       break;
+  if(argc > min_elements_numbers_with_flag && strlen(argv[flag_position]) == flag_length) {
+    first_file_position = 2;
+  }
+  else {
+    first_file_position = 1;
+  }
 
-  //     case TAB_REPLACEMENT_FLAG:
-  //       printf("Выбран верный верный флаг t = %s.\n", optarg);
-  //       break;
+  read_console(first_file_position, argc, argv);
 
-  //     case UNKNOWN_COMMAND_FLAG:
-  //       printf("Неверный ввод!");
-  //       break;
-  //   }
-  // }
+//------------------------------------
+  // clock_gettime(CLOCK_REALTIME, &mt2);
+  // tt = 1000000000 * (mt2.tv_sec - mt1.tv_sec) + (mt2.tv_nsec - mt1.tv_nsec);
+  // printf("Затрачено время : %ld нс\n", tt);  
+//------------------------------------
 
-  return 0;
+return 0;
 }
